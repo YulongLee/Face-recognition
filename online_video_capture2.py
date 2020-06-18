842c6c328554d315067e7628b7b255ff36819dd8
Yulong Lee <47818786+YulongLee@users.noreply.github.com>
2020-06-18 10:30:35 +0800

    Add files via upload

---
+++ online_video_capture2.py
@@ -0,0 +1,18 @@
+# 实时：视频图像采集(dlib)
+import cv2
+import dlib
+
+cap = cv2.VideoCapture(0)
+win = dlib.image_window()
+win.set_title("VideoCapture")
+# 从视频流循环帧
+while cap.isOpened():
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    win.clear_overlay()
+    win.set_image(img)
+    if win.wait_until_closed():
+        break
+cap.release()
